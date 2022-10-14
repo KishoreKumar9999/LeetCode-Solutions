1eadf0ec7278892ae4284b586b49c45aa0e6e4c6
Kishore Kumar <90273016+KishoreKumar9999@users.noreply.github.com>
2022-10-14 18:52:04 -0500

    Time: 585 ms (52.48%), Space: 16.7 MB (18.00%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -0,0 +1,24 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        prefixSumMap = dict()
+        prefixSumMap[0] = 1
+        #print(prefixSumMap)
+        prefixSum = 0
+        ctr = 0
+        for num in nums:
+            prefixSum += num
+            print("PrefixSum:",prefixSum)
+            excess = prefixSum - k
+            print("Excess:", excess)
+            if excess in prefixSumMap.keys():
+                ctr += prefixSumMap[excess]
+            else:
+                ctr += 0
+            if prefixSum in prefixSumMap.keys():
+                prefixSumMap[prefixSum] += 1
+                #print(prefixSumMap)
+            else:
+                prefixSumMap[prefixSum] = 1
+                #print(prefixSumMap)
+        return ctr
+        
