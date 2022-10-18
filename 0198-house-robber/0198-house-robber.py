ed63a57f0fb6756fa7dc67580be1ddd0f438ed0c
Kishore Kumar <90273016+KishoreKumar9999@users.noreply.github.com>
2022-10-18 13:31:53 -0500

    Time: 36 ms (88.71%), Space: 13.8 MB (65.36%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,5 +1,6 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        # rob = max(array[0] + rob[2:n], rpb[1:n])
         rob1, rob2 = 0, 0
         for n in nums:
             maxi = max(n+rob1, rob2)
