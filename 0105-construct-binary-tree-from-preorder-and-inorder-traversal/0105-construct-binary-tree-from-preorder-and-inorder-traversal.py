67c78c5dcc3587f9db963d56ae2a2fa64b304f8e
Kishore Kumar <90273016+KishoreKumar9999@users.noreply.github.com>
2022-10-28 04:26:58 -0500

    Time: 527 ms (21.16%), Space: 376.7 MB (5.74%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -1,17 +1,37 @@
+# TC: O(n^2)
+# SC: O(h)
+# Does this code run on Leetcode: Yes
+# Did you find any difficulty in finding the solution: No
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# class Solution:
+#     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        
+#         def recur(preorder, inorder):
+#             if not preorder and not inorder:
+#                 return None
+#             root = TreeNode(preorder[0])
+#             mid = inorder.index(root.val)
+#             print(mid)
+#             root.left = recur(preorder[1:mid+1], inorder[:mid])
+#             root.right = recur(preorder[mid+1:], inorder[mid+1:])
+#             return root
+#         return recur(preorder, inorder)
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        
         def recur(preorder, inorder):
+            mapping = {}
+            for i,v in enumerate(inorder):
+                mapping[v] = i
             if not preorder and not inorder:
                 return None
             root = TreeNode(preorder[0])
-            mid = inorder.index(root.val)
+            mid = mapping[root.val]
+            print(mid)
             root.left = recur(preorder[1:mid+1], inorder[:mid])
             root.right = recur(preorder[mid+1:], inorder[mid+1:])
             return root
