209229204a3183d8790fd1a49aa191be976c6109
Kishore Kumar <90273016+KishoreKumar9999@users.noreply.github.com>
2022-11-07 14:05:41 -0600

    Time: 432 ms (6.45%), Space: 15.3 MB (98.10%) - LeetHub

---
+++ 0690-employee-importance/0690-employee-importance.py
@@ -0,0 +1,10 @@
+class Solution:
+    def getImportance(self, employees, id):
+        id_to_emp = {employee.id: employee for employee in employees}
+        importance = 0
+        stack = [id_to_emp[id]]
+        while stack:
+            cur_emp = stack.pop()
+            importance += cur_emp.importance
+            stack.extend([id_to_emp[new_emp] for new_emp in cur_emp.subordinates])
+        return importance 
